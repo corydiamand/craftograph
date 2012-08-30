26bac0af2a9739d21f300fbfbc9dcad1aa8602a4
Cory Diamand <corydiamand@gmail.com>
2012-08-30 10:23:40 -0400

    s

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ var zlib = require('zlib');
 var fs = require('fs');
 
 
-
+/*
 fs.readFile('r.0.0.mca', 'utf8', function (err, data) {
   if (err) throw err;
   console.log(data);
 });
+*/
+
+var x = zlib.createInflateRaw({'windowBits':8})
 
 
+for (y in x){
+	console.log(x);
+}
 
-
-
+console.log(x._offset);
